22e502a71b08a5da0cc872269db9f59fb7164b1e
Guillaume Outters <guillaume-scripts@outters.eu>
2007-06-09 11:55:05 +0200

    passelogitelnet: CreamMonkey pour la SocGen
    = Tentative de script pour se connecter directement à la SocGen.
    
    darcs-hash:979009c7533007f5dd7d2de9f044646412db1f60

---
+++ scripts/reseau/logitelnet/passelogitelnet.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name          Passe Logitelnet
+// @namespace     http://localhost
+// @description   Remplit le mdp logitelnet
+// @include       https://logitelnet.socgen.com/
+// @include *
+// ==/UserScript==
+
+document.body.style="background: red;";
+alert('couo');
+setTimeout('alert("miam")', 0);
+//alert(document.forms['authentification']);
