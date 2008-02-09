14b913ca2510ca1a185a280e06b348c9a91713ca
Guillaume Outters <guillaume-scripts@outters.eu>
2008-02-09 22:18:37 +0100

    balance_secu: clé partagée
    + Implémentation d'exemple, fonctionnelle, de balance_secu.
    = Fait un md5 sur une clé partagée et la date actuelle (arrondie à l'heure).
    
    darcs-hash:831651402cdc425cfa823e87b786ab9e445683d8

---
+++ scripts/reseau/balance_secu.php
@@ -0,0 +1,29 @@
+<?php
+
+class Secu
+{
+	private static $mdp = 'clepartagee';
+	
+	function calculer($quand)
+	{
+		return md5(gmstrftime('%Y%m%d%H', $quand).Secu::$mdp); // Précision à l'heure, pour limiter la plage de piratage possible… tout en permettant un minimum de désynchro entre client et serveur.
+	}
+	
+	function verifier()
+	{
+		$v = array();
+		foreach(array(0, -1, 1) as $decalage) // Tout le monde n'est pas à la seconde près, et une seconde, arrondie à l'heure, ça peut faire beaucoup, voire une heure; alors on cherche si les heures voisines n'ont pas servi.
+			if($_POST['mdp'] == Secu::calculer(time() + 3600 * $i))
+				return true;
+		return false;
+		
+		return in_array($_POST['mdp'], $v);
+	}
+	
+	function authentifier(&$requeteCurl, &$params)
+	{
+		$params = array_merge(array('mdp' => Secu::calculer(time())), $params);
+	}
+}
+
+?>
