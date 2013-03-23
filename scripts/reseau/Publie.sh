c97e7367f72eae6373f6884aeddaedd4feba0c1f
Guillaume Outters <guillaume-scripts@outters.eu>
2013-03-23 19:46:28 +0100

    Publie.sh: retaillage
    + On retaille images et vidéos.
    = Affichage des URL générées, des fois qu'on oublie de faire un Pomme-V juste après, on pourra ainsi toujours aller le rerécupérer dans le terminal.
    
    darcs-hash:6d8c413aeb696f7efe3defff118f6fdb0cd6ae40

--- scripts/reseau/Publie.sh
+++ scripts/reseau/Publie.sh
@@ -1,19 +1,64 @@
 #!/bin/sh
 
 liste="$1"
+sed -e 's#/*$##' < "$liste" > "$liste.1"
 http="http://dd.gclo.fr"
 
 listeEnPressePapier()
 {
 	# COPIE: eu.outters.fichiersCopiesEnPressePapier
-	sed -e 's#/*$##' < "$liste" | php -r 'while(($l = fgets(STDIN)) !== false) { echo "'"$http"'/".rawurlencode(basename(rtrim($l, "\n")))."\n"; }' | pbcopy
+	php -r 'while(($l = fgets(STDIN)) !== false) { echo "'"$http"'/".rawurlencode(basename(rtrim($l, "\n")))."\n"; }' < "$liste.1" | pbcopy
+	echo ----------------
+	pbpaste
+	echo ----------------
 	echo "Les futures URL définitives sont copiées, vous pouvez faire un Pomme-V pour les insérer dans un mél. Cependant les URL ne seront véritablement actives que lorsque le transfert qui suit sera terminé." >&2
 }
 
+retaillerImage()
+{
+	while read i
+	do
+		mogrify -resize 800x800 "$i"
+	done
+}
+
+retaillerFilm()
+{
+	while read i
+	do
+		ffmpeg -loglevel panic -strict experimental -i "$i" -vf scale='-1:min(640\, ih*640/iw)' -y -strict experimental "$i.2.MOV" && mv "$i.2.MOV" "$i"
+	done
+}
+
+convertir()
+{
+	moinsDeNMega=3
+	taille="`tr '\012' '\000' < "$liste.1" | xargs -0 du -cm | tail -1 | cut -f 1`"
+	if [ $taille -ge $moinsDeNMega ]
+	then
+		while read fichier
+		do
+			tmp=/tmp/temp.$$.Publie.copie # Sur Mac, on est sur le même disque, donc on peut faire des liens en dur.
+			d="`dirname "$fichier"`"
+			f="`basename "$fichier"`"
+			mkdir -p "$tmp"
+			( cd "$d" && find "$f" -print | cpio -p -al "$tmp" )
+			echo "$tmp/$f"
+			( cd "$tmp" && find "$f" -name \*.MOV | retaillerFilm )
+			( cd "$tmp" && find "$f" -name \*.JPG -or -name \*.jpg | retaillerImage )
+		done < "$liste.1" > "$liste.2"
+		mv "$liste.2" "$liste.1"
+		# À FAIRE: après le transfert, effacer nos fichiers temporaires.
+	fi
+}
+
+# Concrètement, on peut établir la liste dès maintenant, car convertir() remplacera les fichiers sans changer leur nom, donc les URL résultantes.
 listeEnPressePapier
 
-echo "gclo.fr:internet/partage/" >> "$liste"
-sed -e 's#/*$##' < "$liste" | tr '\012' '\000' | xargs -0 rsync -av --progress
+convertir
+
+echo "gclo.fr:internet/partage/" >> "$liste.1"
+tr '\012' '\000' < "$liste.1" | xargs -0 rsync -av --progress
 echo "Transfert terminé. Vous pouvez terminer le programme en faisant un Ctrl-C. Cette fenêtre s'éteindra sinon d'elle-même d'ici une heure." >&2
 sleep 3600
-rm "$liste"
+rm "$liste" "$liste.1"
