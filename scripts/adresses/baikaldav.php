7639b33d6b85dd61993dd7bb21d2770f0a0a3640
Guillaume Outters <guillaume-scripts@outters.eu>
2018-09-29 00:00:01 +0200

    adresses/baikaldav: fiche() peut poster, aussi
    + fiche() sert désormais aussi à envoyer de la donnée.
    + Elle renvoie maintenant un objet plutôt qu'une chaîne (pour pouvoir porter son uri, par exemple).
    
    darcs-hash:82ee63d9f79d647457e8bbadccd9b88e99655c8a

--- scripts/adresses/baikaldav.php
+++ scripts/adresses/baikaldav.php
@@ -1,5 +1,13 @@
 <?php
 
+class FicheDav
+{
+	public function __toString()
+	{
+		return $this->contenu;
+	}
+}
+
 class BaïkalDav
 {
 	public function __construct($url, $qui, $mdp)
@@ -9,30 +17,47 @@ class BaïkalDav
 		$this->mdp = $mdp;
 	}
 	
-	public function fiche($uri)
+	public function fiche($uri, $méthode = 'GET', $contenu = null)
 	{
 		$enTêtes = array
 		(
 			'User-Agent: Mimine 1.0',
 		);
+		if($contenu)
+		{
+			$enTêtes[] = 'Content-Type: text/vcard';
+		}
 		
 		$c = curl_init($this->url.(substr($this->url, -1) == '/' ? '' : '/').urlencode($this->qui).'/default/'.$uri.'.vcf');
 		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($c, CURLOPT_HEADER, 1);
-		curl_setopt($c, CURLOPT_CUSTOMREQUEST, "GET");
-		curl_setopt($c, CURLOPT_HTTPHEADER, $enTêtes);
+		curl_setopt($c, CURLOPT_CUSTOMREQUEST, $méthode);
 		curl_setopt($c, CURLOPT_USERPWD, $this->qui.':'.$this->mdp);
+		curl_setopt($c, CURLOPT_HTTPHEADER, $enTêtes);
 		curl_setopt($c, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+		if($contenu)
+		{
+			curl_setopt($c, CURLOPT_POST, 1);
+			curl_setopt($c, CURLOPT_POSTFIELDS, $contenu->__toString());
+		}
 		$r = curl_exec($c);
-		curl_close($c);
 		
 		if($r === false)
 		{
 			fprintf(STDERR, '#'.curl_error($c)."\n");
+			curl_close($c);
 			return null;
 		}
 		
-		return $r;
+		curl_close($c);
+		
+		if(!$r)
+			return null;
+		
+		$f = new FicheDav();
+		$f->uri = $uri;
+		$f->contenu = $r;
+		
+		return $f;
 	}
 }
 
