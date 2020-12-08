80ac68cc7e0e8bfb267e8f37fb247597f2ef50b7
Guillaume Outters <guillaume-scripts@outters.eu>
2020-12-08 23:10:06 +0100

    reseau/servicenow/reordo.user.js
    + De quoi forcer l'ordre des couleurs, car cet abruti de ServiceNow affecte les couleurs dans l'ordre dans lequel arrivent les items, ce qui fait que si on a fait une jolie palette du rouge au vert (pas bon -> excellent) mais qu'un "excellent" arrive en premier, on a excellent en rouge, pourri en orange, moyen en jaune, et bon en vert.
    - Pour le moment exécution manuelle dans une console.
    
    darcs-hash:8e76345e19647706d1b5ed4ca0f2fd089dab7909

---
+++ scripts/reseau/servicenow/reordo.user.js
@@ -0,0 +1,35 @@
+var treo = [ /Transmis/, /En traitement/, /En attente/, /Résolu/ ];
+var freo = function(bloc)
+{
+	var rects = bloc.querySelectorAll('.highcharts-column-series .highcharts-point');
+	var cols = {};
+	var i, j, l, x, y, r, col, miny;
+	for(i = rects.length; --i >= 0;)
+	{
+		r = rects[i];
+		x = r.x.baseVal.valueAsString;
+		if(typeof cols[x] == 'undefined')
+			cols[x] = [];
+		l = r.getAttribute('aria-label');
+		for(j = -1; ++j < treo.length && !treo[j].match(l);) {}
+		r.poids = j;
+		cols[x].push(r);
+	}
+	for(col in cols)
+	{
+		col = cols[col];
+		if(col.length < 1) continue;
+		col.sort(function(a, b) { return b.poids - a.poids; }); // Ordre inverse, les y les plus faibles étant l'état le plus final.
+		miny = col[0].y.baseVal.value;
+		for(i = 0; ++i < col.length;)
+			if((y = col[0].y.baseVal.value) < miny)
+				miny = y;
+		for(y = miny, i = -1; ++i < col.length;)
+		{
+			col[i].setAttribute('y', y);
+			y += col[i].height.baseVal.value;
+		}
+	}
+};
+
+document.querySelectorAll('.highcharts-series-group').forEach(freo);
