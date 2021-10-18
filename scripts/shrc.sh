f6b797a1ef428b47f070f06c62a2c956cb6862a1
Guillaume Outters <guillaume-scripts@outters.eu>
2021-10-18 20:33:08 +0200

    shrc: commentaire
    - Une section mieux matérialisée.
    
    darcs-hash:6806b536e33a6fbc8b16dbc306ea9ccf73024006

--- scripts/shrc.sh
+++ scripts/shrc.sh
@@ -19,6 +19,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+#- Processus -------------------------------------------------------------------
+
 # Arbre des Processus
 # Similaire au ps -faux Linux.
 # Utilisation: ap [<filtre>]
