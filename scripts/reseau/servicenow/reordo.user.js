693ba2bf93531d599e2572d9cee947c2468a1644
Guillaume Outters <guillaume-scripts@outters.eu>
2022-01-10 23:53:42 +0100

    reseau/servicenow/reordo: lreo
    + On réordonne aussi la légende des aux graphes.
    
    darcs-hash:79096df422491642b3bf3b59cab6acaee09cfa6f

--- scripts/reseau/servicenow/reordo.user.js
+++ scripts/reseau/servicenow/reordo.user.js
@@ -1,7 +1,13 @@
 var treo = [ /Transmis/, /En traitement/, /En attente/, /Résolu/ ];
 var creo = [ [ 255, 127, 0 ], [ 255, 152, 25 ], [ 223, 223, 0 ], [ 143, 175, 0 ] ];
+
+// treo = Tableau de Remise En Ordre; creo = Couleurs; freo = Fonction; lreo = Légende.
+var exyt = /^translate\(([^,]*),([^,]*)\)$/;
+var lreo;
 var freo = function(bloc)
 {
+	// On réordonne les graphes selon treo.
+	
 	var rects = bloc.querySelectorAll('.highcharts-column-series .highcharts-point');
 	var cols = {};
 	var i, j, l, x, y, r, col, miny;
@@ -32,6 +38,39 @@ var freo = function(bloc)
 			y += col[i].height.baseVal.value;
 		}
 	}
+	
+	// Idem pour la légende.
+	
+	lreo(bloc.parentElement.querySelector('.highcharts-legend'));
+};
+lreo = function(bloc)
+{
+	var rects = bloc.querySelectorAll('.highcharts-column-series');
+	var fs = []; // Flottants. Trucs à déplacer.
+	var ps = []; // Positions. Position de chaque bloc, glanées puis reréparties.
+	var i, l, r, trans;
+	for(i = rects.length; --i >= 0;)
+	{
+		r = rects[i];
+		l = r.querySelector('tspan').innerHTML;
+		for(j = -1; ++j < treo.length && !treo[j].test(l);) {}
+		r.poids = j;
+		fs.push(r);
+		trans = r.getAttribute('transform');
+		trans = { t: trans, x: parseFloat(trans.replace(exyt, '$1')), y: parseFloat(trans.replace(exyt, '$2')) };
+		ps.push(trans);
+		
+		r = r.querySelector('rect');
+		r.setAttribute('fill', 'rgb('+creo[j].join(',')+')');
+	}
+	ps.sort(function(a, b) { return a.y == b.y ? a.x - b.x : a.y - b.y; });
+	fs.sort(function(a, b) { return a.poids - b.poids; });
+	for(i = -1; ++i < fs.length;)
+		fs[i].setAttribute('transform', ps[i].t);
+	// À FAIRE: en fait on ne peut reprendre telle quelle l'abcisse: il faut recalculer ces dernières en fonction de la largeur des éléments qu'on a placés devant.
+	// Là où ça se complique, c'est sur du multi-colonnes (ex.: 5 libellés de tailles diverses, répartis sur 2 colonnes):
+	// il faut alors déterminer la largeur max de chaque colonne (/!\ pas séquentiel, la colonne 0 comporte les 0 - 2 - 4 tandis que la 1 contient les 1 - 3).
+	// De plus attention à ce que ça tienne encore dans le contenant (si SNow avait réparti Riquiqui, Petit, Long, Gigantesque en 2 lignes R - L et G - P, le R et le G étaient sur la même large colonne (mettons 150px), et le L et le P sur une autre (mettons 100px), pour un total de 250px. Si maintenant R et G sont chacun sur une colonne, les *deux* colonnes devront faire 150px, pour un total de 300px: rentré-ce encore?).
 };
 
 document.querySelectorAll('.highcharts-series-group').forEach(freo);
