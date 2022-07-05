a2918d78ebdce5cb73cef9a7f8c9d966ee501f72
Guillaume Outters <guillaume-scripts@outters.eu>
2022-07-05 06:23:30 +0200

    omp: Raspberry Pi
    = Quelques macros pour que les #include acceptent d'exposer leurs interfaces POSIX.
    
    darcs-hash:0424ae70eee36bb5949aff5975824ce79f35b7bf

--- scripts/omp.c
+++ scripts/omp.c
@@ -29,7 +29,10 @@
 
 // Permet d'enrober un psql (ou un pg_dump, en lur balançant < /dev/null pour qu'il n'attende pas éternellement une entrée) pour que la saisie de mot de passe se fasse automatiquement depuis le programme, plutôt que de requérir une saisie dans le terminal.
 #ifndef _XOPEN_SOURCE
-#define _XOPEN_SOURCE
+#define _XOPEN_SOURCE 600
+#endif
+#ifndef _DEFAULT_SOURCE
+#define _DEFAULT_SOURCE
 #endif
 #include <stdlib.h>
 #include <fcntl.h>
