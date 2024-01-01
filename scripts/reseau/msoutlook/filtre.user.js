c011d016002ceefa0c5ef8ed45c05319a41f6908
Guillaume Outters <guillaume-scripts@outters.eu>
2024-01-01 18:30:28 +0100

    reseau/msoutlook/filtre.user.js
    = Filtrage fin sur Outlook version web.
    # Non fonctionnel (manque des temporisations)!
    
    darcs-hash:9fae1dc7bc10f266c37a103244d1b6665f18bae7

---
+++ scripts/reseau/msoutlook/filtre.user.js
@@ -0,0 +1,193 @@
+// ==UserScript==
+// @name         Filtrage JS
+// @namespace    http://outters.eu/
+// @description  Ces gros débiles de MS virent le support VBA, ne proposent pas de remplacement JS, mais n'ont même pas songé à offrir un filtre "l'objet est exactement blabla".
+// @grant        none
+// @include      https://outlook.office365.com/*
+// ==/UserScript==
+
+var Dests =
+{
+	p: 'Éléments supprimés',
+	…
+};
+
+var Règles = function(objet, émetteur)
+{
+	switch(objet)
+	{
+		case …: return Dests.p;
+	}
+};
+
+(
+function()
+{
+	// Ensemble de classes CSS porté par les <span> où figure l'objet du mél.
+	var classeObjet = '';
+	var classesExpéditeur = [];
+	var sélecteurObjet;
+	var sélecteurExpéditeur;
+	
+	var info = function(m)
+	{
+		console.log('Filtrage JS: '+m);
+	};
+	
+	// Détermination de l'association de classes désignant l'objet.
+	// On sait qu'objet et émetteur ont tous deux un title, toujours vide pour l'objet, et comportant l'adrél pour l'émetteur, mais uniquement lorsque l'adrél a pu être interprétée et validée par Outlook.
+	var détection = function()
+	{
+		var classes = {};
+		var nobles = document.querySelectorAll('div[class] > span[title]');
+		nobles.forEach(function(noble)
+		{
+			var noblesse = noble.getAttribute('title');
+			var typos, typo;
+			if(noblesse === '')
+				typos = { 'adrél': 1, 'objet': 1 }; // En fait "soit une adrél, soit un objet".
+			else if(/@/.test(noblesse))
+				typos = { 'adrél': 1 };
+			if(undefined === typos) return;
+			
+			var classe = noble.parentNode.getAttribute('class');
+			if(undefined === classes[classe]) classes[classe] = {};
+			for(typo in typos)
+				classes[classe][typo] = (undefined === classes[classe][typo] ? 0 : classes[classe][typo]) + typos[typo];
+		});
+		var classe, nObjets = 0, nAdrels = 0;
+		// Les objets ont toujours la même classe, tandis que les adréls changent selon que le mél est lu ou non.
+		// En outre les assimilés objet peuvent être des adréls vides.
+		// On repère donc tout d'abord le groupe de classes le plus donné, qui devrait désigner les objets.
+		for(classe in classes)
+			if(classes[classe].objet == classes[classe].adrél && classes[classe].objet > nObjets)
+				nObjets = classes[classe].objet;
+		if(!nObjets) return info('Impossible de repérer les objets de mél');
+		for(classe in classes)
+			if(classes[classe].objet == classes[classe].adrél && classes[classe].objet == nObjets)
+			{
+				if(classeObjet) return info('Ouille, plusieurs libellés pourraient porter les objets de mél');
+				classeObjet = classe;
+			}
+			else
+			{
+				nAdrels += classes[classe].adrél;
+				classesExpéditeur.push(classe);
+			}
+		if(nAdrels != nObjets) return info('Je ne trouve '+nObjets+' objets de mél pour '+nAdrels+' expéditeurs');
+		
+		info('Éléments des méls repérés');
+		
+		return classeObjet;
+	}
+	
+	var destinations = function()
+	{
+		var d, é;
+		for(d in Dests)
+		{
+			if(typeof(Dests[d]) == 'string')
+			//querySelectorAll('[draggable]')
+			// https://stackoverflow.com/a/37098628
+			if(!(é = document.evaluate('//span[text()="'+Dests[d]+'"]', document, null, XPathResult.ANY_TYPE, null).iterateNext())) return info('Boîte "'+d+'" introuvable');
+			// Le remplacement suivant était destiné au glisser-déposer. Mais comme on ne sait pas le faire marcher, le fonctionnement par menu contextuel nous fait garder le simple libellé.
+			//Dests[d] = é;
+		}
+		
+		info('Destinations repérées');
+		return true;
+	};
+	
+	var menu = function(nom)
+	{
+		var r;
+		// Le menu premier niveau est en itemText; le second niveau est un gros foutoir de div et span (pour avoir des jolies icônes?).
+		document.querySelectorAll('.ms-ContextualMenu-itemText, .ms-ContextualMenu-item div span').forEach(function(x) { if(x.innerText == nom && !r) r = x; });
+		return r;
+	};
+	
+	var souris = function(cible, quoi)
+	{
+		var ops =
+		{
+			click:
+			[
+				{ op: 'mouseover' },
+				{ op: 'mousedown' },
+				{ op: 'mouseup' },
+				{ op: 'click' }
+			],
+			contextmenu:
+			[
+				{ op: 'mouseover', opts: { button: 2 } },
+				{ op: 'mousedown', opts: { button: 2 } },
+				{ op: 'mouseup', opts: { button: 2 } },
+				{ op: 'contextmenu', opts: { button: 2 } }
+			],
+			mouseover:
+			[
+				{ op: 'mouseover' }
+			]
+		};
+		
+		var op = ops[quoi];
+		if(!op) op = [ { op: op } ];
+		
+		for(var i = -1; ++i < op.length;)
+		{
+			var opts = { view: window, bubbles: true, cancelable: true };
+			if(op[i].opts)
+				for(var opt in op[i].opts)
+					opts[opt] = op[i].opts[opt];
+			var ev = new MouseEvent(op[i].op, opts);
+			cible.dispatchEvent(ev);
+		}
+	};
+	
+	var tourner = function()
+	{
+		var bal;
+		document.querySelectorAll('span.screenReaderOnly').forEach(function(x)
+		{
+			if(x.innerText == 'sélectionné')
+				bal = x.parentNode.querySelector('div > span').innerText;
+		});
+		if(bal != 'Boîte de réception') return;
+		
+		document.querySelectorAll('[title="Marquer comme lu"]').forEach(function(x)
+		{
+			while((x = x.parentNode) && x.getAttribute && !x.getAttribute('data-animatable')); if(!x) return;
+			var objet = x.querySelector(sélecteurObjet).innerText;
+			var expé = x.querySelector(sélecteurExpéditeur);
+			var dest = Règles(objet, expé);
+			if(dest)
+			{
+				var de = x.querySelectorAll('[title]')[2]; // Surtout pas le [0], qui est le marqueur "lu / non lu"; le 1 est l'expéditeur, le 2 le titre.
+				souris(de, 'contextmenu');
+				// Tempo à mettre?
+				souris(menu('Déplacer'), 'mouseover');
+				// Tempo à mettre?
+				souris(menu('Déplacer vers un autre dossier...'), 'click');
+				souris(menu(dest), 'click');
+				info(objet+' → '+dest.innerText);
+			}
+		}); 
+	};
+	
+	var installer = function()
+	{
+		if(!classeObjet && !détection()) return;
+		if(!destinations()) return;
+		
+		window.clearInterval(scrutateur);
+		
+		sélecteurObjet = '.'+classeObjet.replaceAll(/ +/g, '.')+' [title]';
+		sélecteurExpéditeur = classesExpéditeur.map(x => '.'+x.replaceAll(/ +/g, '.')+' [title]').join(', ');
+		
+		tourner();
+		window.setInterval(tourner, 10000);
+	};
+	var scrutateur = window.setInterval(installer, 1000);
+}
+)
+();
