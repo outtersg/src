1c8a158cdd9c828963f7998255429a9822d0a381
Guillaume Outters <guillaume-scripts@outters.eu>
2024-08-06 23:07:45 +0200

    reseau/msteams/contacts.user.js: v2
    = MS a changé les classes CSS par lesquels accéder.
    + On affiche l'intégralité des participants d'une conversation de groupe.
    
    darcs-hash:b10bbaaca366a9df3b2ff1195b5b595d311ad258

--- scripts/reseau/msteams/contacts.user.js
+++ scripts/reseau/msteams/contacts.user.js
@@ -22,7 +22,12 @@ var retapages =
 	'.app-participant-unit.single-line-truncation', // Interloc unique en tête d'une conversation passée en premier plan.
 	'.app-participant-unit > span[pl-name]', // Interloc multiple en tête d'une conversation passée en premier plan.
 	'.ui-chat__message__author > .ui-text', // Dans une conversation, auteur d'un message en particulier.
-	'.ms-Persona > .ms-Persona-primaryText > .ms-TooltipHost'
+	'.ms-Persona > .ms-Persona-primaryText > .ms-TooltipHost', // Détail d'un nom de conversation "MACHIN +2"
+	// v2
+	'[data-tid="chat-list-item-title"]', // Nom de conversation
+	'[id^="chat-topic-person"]', // Interloc en tête d'une conversation passée en premier plan.
+	'[data-tid="message-author-name"]' // Dans une conversation, auteur d'un message en particulier.
+	/* À FAIRE: dans le fil de conversation, sous son titre apparaît le dernier message qui commence par un "NOM, Prénom: Bla bla": on pourrait remplacer aussi ce NOM, Prénom. */
 	// À FAIRE: pour les "a ajouté Untel à la conversation", j'ai un .bko, mais n'est-ce pas un nom de classe regénéré à chaque fois?
 	// À FAIRE: pour les interpellations d'une personne dans une conversation, Teams découpe son nom en autant de liens que de mots :-( donc il faudrait repérer les blocs successifs et les remplacer.
 ];
@@ -35,7 +40,29 @@ var retaperUn = function(document)
 		if(i.dejaFait == i.innerHTML) return;
 		i.dejaFait = i.innerHTML;
 		
-		if(unsafeWindow.retape[i.innerHTML]) i.innerHTML = unsafeWindow.retape[i.innerHTML];
+		var composite, r, icone;
+		
+		// Pour les conversations, le "NOM, Prénom, +2" n'est pas satisfaisant.
+		// On va chercher la liste des participants dans l'icône "+ d'infos sur la conversation".
+		if((icone = i.parentElement.parentElement.parentElement.querySelector('img[role="presentation"]')))
+			if((icone = new URL(icone.src).searchParams.get('usersInfo')))
+				if((icone = JSON.parse(icone)))
+				{
+					composite = [];
+					for(var j = -1; ++j < icone.length;)
+						composite.push(unsafeWindow.retape[r = icone[j].displayName] || r);
+					// Condition: que le titre de conversation n'ait pas été personnalisé,
+					// autrement dit qu'il commence par le premier participant.
+					var prems = icone[0].displayName;
+					if(i.innerHTML.substr(0, prems.length) != prems) return;
+					r = composite.join(' / ');
+				}
+		
+		if(!r) r = unsafeWindow.retape[i.innerHTML];
+		if(!r && (composite = i.innerHTML.match(/,( \+[0-9]+)$/)) && (r = unsafeWindow.retape[i.innerHTML.replace(composite[0], '')]))
+			r += composite[1];
+
+		if(r) i.innerHTML = r;
 	});
 };
 
