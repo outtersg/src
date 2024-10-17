247de0042754d93bd59c15c7ec19a3cf7aa1878d
Guillaume Outters <guillaume-scripts@outters.eu>
2024-10-17 07:14:34 +0200

    jira/spacieux: modularisation
    - On déplace chaque fonctionnalité dans une fonction propre, histoire d'avoir dans inst() le déroulé concis de toutes les modifs qu'on applique successivement.
    
    darcs-hash:9af1a437f326dade7faaf75c8ac403705ece3ecd

--- scripts/reseau/jira/spacieux.js
+++ scripts/reseau/jira/spacieux.js
@@ -33,51 +33,38 @@ var gira =
 		req.open('GET', giraIncsUrl.replace(/@NUM/, num).replace(/@REFS/, refs));
 		req.send();
 	},
-	/* Les listes déroulantes ont un [max-]width: 100% qui prend trop de place une fois qu'on supprime le menu droite;
-	 * donc visuellement déjà ça fait moche, mais pire: les suggestions du menu s'affichant à droite du menu lui-même, tombent hors page lorsque le menu prend toute la largeur! */
-	deroulantes: function(css)
+	menudroite: function(gp /* grand-père */)
 	{
-		css.appendChild(document.createTextNode('.full-width { max-width: 512px; }'));
-	}
-};
-
-{
-	var ints;
-	var inst = function()
-	{
-		var style = '';
-		var tete = document.getElementsByTagName('head')[0];
-		var gp = document.querySelector('.issue-body-content, [data-testid="issue.views.issue-details.issue-layout.container-left"]');
-		if(gp) window.clearInterval(ints); else return;
-		document.querySelector('[data-testid="issue.views.issue-details.issue-layout.container-right"]').id = 'viewissuesidebar';
-		var pepere = document.getElementById('viewissuesidebar');
+		var pepere = document.querySelector('[data-testid="issue.views.issue-details.issue-layout.container-right"]');
+		if(pepere) pepere.id = 'viewissuesidebar';
+		else pepere = document.getElementById('viewissuesidebar');
 		var menu = document.createElement('DIV');
 		var zorro = document.createElement('DIV');
 		var i;
-		
-		var commentaireFlottant = document.querySelector('[data-testid="issue.activity.comment"]');
-		if(commentaireFlottant)
-			commentaireFlottant.parentNode.parentNode.style.setProperty('position', 'static'); // Au lieu de sticky.
 		
 		var agrandissement = document.querySelector('[data-component-selector="jira-issue-view-common-component-resize-handle"]');
 		if(agrandissement)
 			for(i = agrandissement.classList.length; --i >= 0;)
 				if(document.querySelectorAll('.'+agrandissement.classList[i]).length == 1)
-					style += '.'+agrandissement.classList[i]+'[role="separator"] { display: none; }\n';
+					gira.style.innerHTML += '.'+agrandissement.classList[i]+'[role="separator"] { display: none; }\n';
+		
 		menu.setAttribute('class', 'pouet');
 		zorro.setAttribute('class', 'zorro');
-		zorro.setAttribute('style', 'display: inline-block; width: 32px; height: 32px; right: 0; background: url('+document.querySelectorAll('#assignee-val img, [data-testid="issue.views.field.user.assignee"] img')[0].src+') no-repeat center center; background-size: contain;');
 		pepere.setAttribute('style', 'width: 400px; padding: 0;');
 		menu.appendChild(zorro);
 		menu.appendChild(pepere);
 		gp.insertBefore(menu, gp.childNodes[0]);
 		
-		style +=
+		gira.style.innerHTML +=
 			'.pouet { display: inline-block; position: absolute; right: 0; z-index: 256; width: auto; padding: 5px; border: 1px solid #DFDFDF; background: white; }\n'+
 			'.pouet #viewissuesidebar { display: none; max-height: 400px; }\n'+
 			'.pouet:hover #viewissuesidebar { display: block; }\n'+
-			'.pouet .zorro { position: relative; }\n'+
+			'.pouet .zorro { position: relative; display: inline-block; width: 32px; height: 32px; right: 0; background: url('+document.querySelectorAll('#assignee-val img, [data-testid="issue.views.field.user.assignee"] img')[0].src+') no-repeat center center; background-size: contain; }\n'+
 			'.pouet:hover .zorro { position: absolute; }\n';
+	},
+	densifier: function()
+	{
+		// Densité de texte.
 		
 		// Recherche de la classe CSS en tête de tout le contenu.
 		var unTitre = document.querySelector('[class^="css-"] h2[data-testid="issue.views.issue-base.common.description.label"]');
@@ -86,27 +73,68 @@ var gira =
 		{
 			while(unTitre && !(unTitre.className && unTitre.className.match(/^css-/))) unTitre = unTitre.parentNode;
 			if(unTexte) // Marcherait aussi avec unTitre finalement.
-				style +=
+				gira.style.innerHTML +=
 					'.'+unTexte.classList[0]+' p { line-height: 1.1 !important; letter-spacing: 0 !important; }\n'+
 					'.'+unTexte.classList[0]+' { font-size: 9pt; }\n';
 		}
 		document.body.style.setProperty('--ds-font-body', '9pt sans-serif'); // Celui-là est paramétrable par une simple pseudo-prop du corps, ouf!, qui passe avant tout le fatras qu'ils ont définir par défaut.
-		style += '.code-block code[class*="language-"] { font-size: 9pt; line-height: 1; }\n';
+		gira.style.innerHTML += '.code-block code[class*="language-"] { font-size: 9pt; line-height: 1; }\n';
 		
-		// Les réactions: on va choper la classe "privée" du conteneur.
+		// Les émoticônes c'est un plus mais pas au point de devoir bouffer une ligne de texte.
+		
+		// On chope la classe "privée" du conteneur.
 		var uneReac = document.querySelector('[data-testid="issue-comment-base.ui.comment.reactions-container"]');
 		if(uneReac)
 		{
 			uneReac = uneReac.parentNode;
-			style +=
+			gira.style.innerHTML +=
 				'.'+uneReac.classList[0]+' { position: absolute; bottom: 0; left: 0; opacity: 0.33; }\n'+
 				'.'+uneReac.classList[0]+':hover { z-index: 2; opacity: 1; background: var(--ds-surface, white); }\n'+
 				'.'+uneReac.previousSibling.classList[0]+' { z-index: 1; }'+
 				'.'+uneReac.previousSibling.querySelector('h3').classList[0]+' { font-size: 80%; }';
 		}
+	},
+	/* Qu'est-ce que c'est que cette incitation à commenter sans même avoir lu les commentaires de nos prédecesseurs? */
+	tourner7fois: function()
+	{
+		var commentaireFlottant = document.querySelector('[data-testid="issue.activity.comment"]');
+		if(commentaireFlottant)
+			commentaireFlottant.parentNode.parentNode.style.setProperty('position', 'static'); // Au lieu de sticky.
+	},
+	/* Les listes déroulantes ont un [max-]width: 100% qui prend trop de place une fois qu'on supprime le menu droite;
+	 * donc visuellement déjà ça fait moche, mais pire: les suggestions du menu s'affichant à droite du menu lui-même, tombent hors page lorsque le menu prend toute la largeur! */
+	deroulantes: function(css)
+	{
+		css.appendChild(document.createTextNode('.full-width { max-width: 512px; }'));
+	},
+	/* Et pourquoi notre société considérerait-elle que les vieux sont sans intérêt et doivent être masqués?
+	 * (bon d'accord ce ne sont que de vieux *commentaires*) */
+	beguinage: function()
+	{
+		var plus = document.querySelector('[data-testid="issue.activity.common.component.load-more-button.loading-button"]');
+		if(plus) plus.click();
+	}
+};
+
+{
+	var ints;
+	var inst = function()
+	{
+		var tete = document.getElementsByTagName('head')[0];
+		var gp = document.querySelector('.issue-body-content, [data-testid="issue.views.issue-details.issue-layout.container-left"]');
+		if(gp) window.clearInterval(ints); else return;
 		
-		tete.insertAdjacentHTML('beforeend', '<style type="text/css">\n'+style+'</style>');
-		var st = tete.lastChild;
+		tete.insertAdjacentHTML('beforeend', '<style type="text/css">\n</style>');
+		gira.style = tete.lastChild;
+		
+		/* Présentation */
+		
+		gira.densifier();
+		gira.menudroite(gp);
+		gira.deroulantes(gira.style);
+		gira.tourner7fois();
+		
+		/* Fonctionnalités */
 		
 		// Accès à AJS pour lui injecter de nouveaux raccourcis clavier.
 		// Pas besoin de ruser façon http://5.9.10.113/58877097/add-a-custom-key-shortcut-within-javascript-in-confluence (car on est déjà chargés une fois la page bien installée).
@@ -135,13 +163,9 @@ var gira =
 		} + ")();";
 		document.body.appendChild(s);
 		
-		gira.deroulantes(st);
 		gira.incs();
 		
-		// Et pourquoi notre société considérerait-elle que les vieux sont sans intérêt et doivent être masqués?
-		// (bon d'accord ce ne sont que de vieux *commentaires*)
-		var plus = document.querySelector('[data-testid="issue.activity.common.component.load-more-button.loading-button"]');
-		if(plus) plus.click();
+		gira.beguinage();
 	};
 	
 	ints = window.setInterval(inst, 1000);
