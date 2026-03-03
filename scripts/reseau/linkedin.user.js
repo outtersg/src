2337028439956f1da79c772d78208bb060fd46ed
Guillaume Outters <guillaume-scripts@outters.eu>
2026-03-03 17:28:14 +0100

    reseau/linkedin.user.js: persistence
    + Ce foutu serveur LinkedIn n'ayant aucune notion de persistence (il oublie que j'ai déjà consulté moult fois tout un tas d'annonces fort inintéressantes: tu charges une liste d'annonces, tu cliques sur les unes et les autres il te les affiche Consultée, tu recharges la page hop mémoire de poisson rouge), on va utiliser le localStorage pour nous rappeler ce qu'on a déjà parcouru.
    = On recalcule toutes les 3 s plutôt que 10 s (les éléments se chargeant à la volée quand on défile).
    
    darcs-hash:05935f9e7c5d48fd8112504a026d4bc14d5b4e9a

--- scripts/reseau/linkedin.user.js
+++ scripts/reseau/linkedin.user.js
@@ -5,6 +5,64 @@
 // @grant     none
 // ==/UserScript==
 
+var AttentisteUrl =
+{
+	attente: function()
+	{
+		if(AttentisteUrl.url != document.URL)
+		{
+			AttentisteUrl.url = document.URL;
+			if(!AttentisteUrl.attenteH2.repet)
+				AttentisteUrl.attenteH2.repet = window.setInterval(AttentisteUrl.attenteH2, 200);
+			AttentisteUrl.attenteH2.compteur = 10;
+			console.log('Embellificateur: attente ID (nouvelle URL: '+document.URL+')');
+		}
+	},
+	attenteH2: function()
+	{
+		var urlId, bloc = document.querySelector('[data-view-name="job-detail-page"], .jobs-details');
+		if
+		(
+			bloc
+			&& bloc.querySelector('h2')
+			&& (urlId = bloc.querySelector('a[href^="/jobs/view/"], a[href*="JobId="]'))
+			&& (urlId = urlId.href.match(/(?:\/jobs\/view\/|JobId=)([0-9]{8,})/))
+			&& (urlId = urlId[1]) != AttentisteUrl.attenteH2.dernierId
+		)
+		{
+			console.log('Embellificateur: ID '+urlId);
+			AttentisteUrl.attenteH2.dernierId = urlId;
+			AttentisteUrl.attenteH2.compteur = 0;
+			if(!localStorage.getItem('vu/'+urlId))
+				localStorage.setItem('vu/'+urlId, new Date().toISOString().substr(0, 10));
+		}
+		if(--AttentisteUrl.attenteH2.compteur < 0)
+		{
+			window.clearInterval(AttentisteUrl.attenteH2.repet);
+			delete AttentisteUrl.attenteH2.repet;
+		}
+	},
+	url: null
+};
+
+/* On aurait voulu intercepter les changements d'URL.
+* https://stackoverflow.com/a/4585031/1346819
+* Cependant nous ne sommes jamais appelés.
+* Tant pis, dans un premier temps on continuera à exploiter le setInterval plus bas.
+(function(history){
+var pushState = history.pushState;
+history.pushState = function(state) {
+urlChange();
+return pushState.apply(history, arguments);
+};
+var replaceState = history.replaceState;
+history.replaceState = function(state) {
+urlChange();
+return replaceState.apply(history, arguments);
+};
+})(window.history);
+*/
+
 (
 	function()
 	{
@@ -13,12 +71,21 @@
 		var marquer = function()
 		{
 			var trouve = 0;
-			document.querySelectorAll('.scaffold-layout__list-item:not([etat="Enregistré"]):has(.job-card-container__footer-job-state)').forEach
+			document.querySelectorAll('[data-job-id]:not([etat])').forEach
 			(
 				function(x)
 				{
 					trouve = 1;
-					x.setAttribute('etat', x.querySelector('.job-card-container__footer-job-state').innerText)
+					let etatAff = x.querySelector('.job-card-container__footer-job-state');
+					let consult = localStorage.getItem('vu/'+x.dataset.jobId);
+					if(etatAff) etatAff = etatAff.innerText;
+					else if(consult) etatAff = 'Consulté';
+					if(etatAff)
+					{
+						console.log('Embellificateur: '+x.dataset.jobId+' '+etatAff);
+						x.setAttribute('etat', etatAff);
+						/* À FAIRE: consult contient la date de première consultation. L'afficher. */
+					}
 				}
 			);
 			return trouve;
@@ -31,10 +98,12 @@
 				if(marquer())
 				{
 					window.clearInterval(attente);
-					window.setInterval(marquer, 10000);
+					window.setInterval(marquer, 3000);
 				}
 			}
 			, 500
 		);
+		window.setInterval(AttentisteUrl.attente, 3000);
+		AttentisteUrl.attente();
 	}
 )();
