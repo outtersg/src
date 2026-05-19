3fa3f1a69a9e0b2803f74b5c963e79a0217a4e84
Guillaume Outters <guillaume-scripts@outters.eu>
2026-05-19 10:06:58 +0200

    reseau/hellowork.user.js
    + Extracteur rapide d'offres HelloWork, sur le modèle de celui pour LinkedIn.
    
    darcs-hash:b0967a5636fab9f5ba10087568ed3d8c66334b8f

---
+++ scripts/reseau/hellowork.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name      Embellificateur HelloWork
+// @namespace http://outters.eu/
+// @include   https://www.hellowork.com/fr-fr/emplois/*
+// @grant     none
+// ==/UserScript==
+
+var copierHelloWork = function()
+{
+	var d = new Date().toISOString().substr(0, 10);
+	var titre = document.querySelector('h1');
+	var poste = titre.querySelector('[data-cy="jobTitle"]').innerText;
+	var boite = titre.querySelector('a[href*="/entreprises/"]').innerText;
+	var ville = document.querySelector('.tw-tag-secondary-s').innerText; // La ville est le premier cartouche d'infos.
+	var salaire = document.querySelector('[data-input-checker-input-value="#salaryModalInformation"] > span').innerText;
+	poste = poste.replaceAll(/[\s-(]*(?:[HMFX](?:[-\/.][HMFX]){1,2})\)?/g, '');
+	var resume =
+		'- ['+boite+'] '+poste+' '+document.URL+' {'+d+'}'+"\n"
+		+ville+"\n"
+		+salaire
+	;
+	navigator.clipboard.writeText(resume);
+};
+
+(
+	function()
+	{
+		copierHelloWork();
+	}
+)();
