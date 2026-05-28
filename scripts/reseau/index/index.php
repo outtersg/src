c11af787e8db8a7fddaf03387e9a12e364b04a39
Guillaume Outters <guillaume-scripts@outters.eu>
2026-05-28 15:49:26 +0200

    reseau/index/index.php: imagettes
    + Les *.jpg sont affichés avec une imagette.
    = Truc rapide: l'imagette est à l'échelle 1/10ème.
    
    darcs-hash:30a2b7a01fdab33a66128266fa66a86a5521c44d

--- scripts/reseau/index/index.php
+++ scripts/reseau/index/index.php
@@ -15,8 +15,23 @@ $fs = glob('*');
 sort($fs);
 $spéciaux = array('.', 'index.php');
 foreach($fs as $f)
-	if(!in_array($f, $spéciaux))
-		echo '<a href="'.rawurlencode($f).'">'.htmlspecialchars($f).'</a><br/>'."\n";
+	if(!in_array($f, $spéciaux) && !preg_match('/.mini.jpg$/', $f))
+	{
+		if(($mini = preg_replace('/\.(jpg)$/', '.mini.jpg', $f)) == $f)
+			$mini = null;
+		else
+			if(!file_exists($mini))
+			{
+				// Sinon magick IMG_20260528_090329.jpg -resize 200 IMG_20260528_090329.mini.jpg
+				$i = imagecreatefromjpeg($f);
+				$h = imagesy($i);
+				$l = imagesx($i);
+				$i2 = imagecreatetruecolor($nl = (int)($l * 0.1), $nh = (int)($h * 0.1));
+				imagecopyresampled($i2, $i, 0, 0, 0, 0, $nl, $nh, $l, $h);
+				imagejpeg($i2, $mini);
+			}
+		echo '<a href="'.rawurlencode($f).'">'.($mini ? '<img src="'.rawurlencode($mini).'"/>' : '').htmlspecialchars($f).'</a><br/>'."\n";
+	}
 
 ?>
 	</body>
