1fb3f5ffb1333a423a02b7ea8bd999d933ad2c60
Guillaume Outters <guillaume-scripts@outters.eu>
2026-06-12 16:50:04 +0200

    reseau/index/index.php: tri version sémantique
    + Petit semver entre les fichiers préfixés par du numérique.
    
    darcs-hash:3e56dc276e15342eb83acd1643c49d23cd12a412

--- scripts/reseau/index/index.php
+++ scripts/reseau/index/index.php
@@ -13,8 +13,26 @@ if(!chdir(dirname(__FILE__))) throw new Exception('Zut, impossible d\'afficher l
 
 $imagettes = 1;
 
+function vercmp($a, $b)
+{
+	preg_match('/^([0-9]+\.)*/', $a, $a);
+	preg_match('/^([0-9]+\.)*/', $b, $b);
+	$a = explode('.', $a[0]);
+	$b = explode('.', $b[0]);
+	foreach($a as $pos => $val)
+	{
+		if(!isset($b[$pos])) return 1;
+		$aval = (int)$val;
+		$bval = (int)$b[$pos];
+		if($bval != $aval) return $aval - $bval;
+	}
+	if(isset($b[$pos + 1])) return -1;
+	return 0;
+}
+
 $fs = glob('*');
-sort($fs);
+usort($fs, 'vercmp');
+//$fs = ['4.truc', '4.1.u', '5.k', '5.1.l', '5.2.m' ]; // Pour tester l'arborescence.
 $spéciaux = [ '.', '..', 'index.php', 'Makefile' ];
 foreach($fs as $f)
 	if(!in_array($f, $spéciaux) && !preg_match('/\.(mini\.jpg|ninja|sh)$/', $f))
