a1011efc2b2ff6afb98c654accad3c0cb18df330
Guillaume Outters <guillaume-scripts@outters.eu>
2026-08-22 21:54:55 +0200

    texte/util.rfc822.sh: rfc822_pjs()
    + Récupère les PJ, avec un filtrage en awk.
    
    darcs-hash:7d2916a0ed4885e3da41fdf59bb12e69e30e9ca5

--- scripts/texte/util.rfc822.sh
+++ scripts/texte/util.rfc822.sh
@@ -17,3 +17,68 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+# Lit un message RFC822, passant chaque PJ à une fonction passée en paramètre.
+# Utilisation: rfc822_pjs <test> < <fichier>
+#   <test>
+#     Test (écrit en awk) invoqué lors du passage des en-têtes au corps de bloc MIME;
+#     si le test est positif (on souhaite récupérer cette PJ), doit mettre dans la variable dest un chemin vers lequel écrire le fichier.
+rfc822_pjs()
+{
+	local test="$1"
+	awk \
+	'
+		# etat:
+		#   0: corps de texte
+		#   1: 1 ligne vide
+		#   2: 1 ligne vide, une ligne -- (début de segment MIME)
+		#   3: en-têtes
+		BEGIN { partie = 0; etat = 3; }
+		function fermer() {
+			if(cumul) { cumul = substr(cumul, 1, length(cumul) - 1); pousser(cumul); cumul = ""; }
+			if(dest)
+				if(decodage)
+					close(decodage);
+				else
+					close(dest);
+		}
+		function pousser(x) {
+			if(!dest) return;
+			if(decodage)
+				print x | decodage;
+			else
+				print x > dest;
+		}
+		/^--/ && etat <= 1 { etat = 2; } # À FAIRE: il peut y avoir des -- qui ne correspondent pas au délimiteur annoncé au départ, à considérer comme du texte brut sans changement d état.
+		/^$/ {
+			if(etat == 3)
+			{
+				etat = 0;
+				dest = "";
+				'"$test"'
+				if(dest)
+				{
+					if(enTetes["Content-Transfer-Encoding"] == "base64")
+						decodage = "'"$HOME/bin/debase64"'";
+					else
+					{
+						print "# Erreur: je devrais pondre vers "dest" avec un Content-Transfer-Encoding de "enTetes["Content-Transfer-Encoding"] > "/dev/stderr";
+						dest = "";
+						decodage = "";
+					}
+					if(dest && decodage)
+						decodage = decodage" > "dest;
+				}
+			}
+			else
+				etat = 1;
+		}
+		etat == 0 && dest {
+			pousser($0);
+		}
+		etat > 0 && etat < 2 { cumul = cumul$0"\n"; }
+		etat == 2 && /: / { ++etat; delete enTetes; fermer(); }
+		etat == 3 && /: / { match($0, /: /); enTetes[substr($0, 1, RSTART - 1)] = substr($0, RSTART + 2); }
+		END { if(dest) fermer(); }
+	'
+}
