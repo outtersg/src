5c21ef96f970de9f87b663fcef4a61d769726fca
Guillaume Outters <guillaume-scripts@outters.eu>
2026-09-24 10:39:34 +0200

    BOGUE: shrc: filtrer() faisait perdre entre autres le droit d'exécution
    # Ma dernière modif oubliait qu'un fichier texte pouvait vouloir être exécutable.
    - On part sur une implémentation un peu coûteuse (cp -p).
    
    darcs-hash:b7dfc8b69fe75d29bc3ed55026a4216c31f8db0b

--- scripts/shrc.sh
+++ scripts/shrc.sh
@@ -200,6 +200,7 @@ filtrer()
 	
 	fichier="$1"
 	shift
+	cp -p "$fichier" "$fichier.filtrer.temp" # Pour conserver notamment les permissions. Dommage si c'est un gros fichier, mais c'est plus simple que de jongler avec stat (stat -f %OLp sur FreeBSD; stat -c %a sur Raspbian), qui en outre couvrirait sans doute moins que l'ensemble des arcanes gérées par cp -p.
 	"$@" < "$fichier" > "$fichier.filtrer.temp" || { local r=$? ; rm -f "$fichier.filtrer.temp" ; return $r ; }
 	case "$preserve" in 1) touch -r "$fichier" "$fichier.filtrer.temp" ;; esac
 	mv "$fichier.filtrer.temp" "$fichier"
